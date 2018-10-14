dd5e1137c10d4ce491bb8d8b3e80d202a6fcfd59
andrii.shaforostov <andpr99@gmail.com>
2018-10-14 16:12:15 +0300

    remove redundant files and folders

--- server/app.js
+++ server/app.js
@@ -1,27 +1,44 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var os = require('os');
+const addrs = require('local-ip-addresses-and-hostnames');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const cookieParser = require('cookie-parser');
+const WebSocket = require('ws');
+// const indexRouter = require('./routes/index');
+const sessions = require('./sessions');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const wss = new WebSocket.Server({
+  port: 3002
+});
 
-var app = express();
-const expressWs = require('express-ws')(app, );
+const wsHandlers = require('./wsHandlers')(wss);
 
-// app.use(logger('dev'));
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-//
-app.get('/', indexRouter);
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.ws('/', (ws, req) => {
-  ws.on('message', (msg) => {
-    console.log(msg);
+// app.use('/', indexRouter);
+
+wss.on('connection', (ws) => {
+  sessions.addConnection(ws);
+  ws.on('message', processMessage);
+  ws.on('close', function (code) {
+    sessions.removeConnection(this.id);
   });
-  console.log('test');
 });
 
-module.exports = app;
+function processMessage(message) {
+  // console.log(message);
+  const { action, payload } = JSON.parse(message);
+  wsHandlers[action](this, payload);
+}
+
+console.log(`ws server is working on:`);
+const ips = addrs.getLocalIpAddresses();
+ips.forEach(ip => console.log(`${ip}:3002`));
